f7015658718798e7d8124c6430c295a47efb6611
Kim-Yiji <yiji_kim@yonsei.ac.kr>
2025-03-06 15:33:27 +0900

    hwo1 upload

--- hw01_2020147527/01_HelloWindow.js
+++ hw01_2020147527/01_HelloWindow.js
@@ -6,11 +6,11 @@ if (!gl) {
     console.error('WebGL 2 is not supported by your browser.');
 }
 
-// Set canvas size: 현재 window 전체를 canvas로 사용
+// Set canvas size: 초기 500x500
 canvas.width = 500;
 canvas.height = 500;
 
-// Enable scissor test to clear only specific areas
+// scissor 설정
 gl.enable(gl.SCISSOR_TEST);
 
 // Initialize WebGL settings: viewport and clear color
@@ -29,28 +29,28 @@ function render() {
     const width = canvas.width / 2;
     const height = canvas.height / 2;
 
-    // 1사분면 (오른쪽 위, 초록색)
+    // 1사분면 (초)
     gl.viewport(width, height, width, height);
     gl.scissor(width, height, width, height);
-    gl.clearColor(0.0, 1.0, 0.0, 1.0);  // 초록
+    gl.clearColor(0.0, 1.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // 2사분면 (왼쪽 위, 빨간색)
+    // 2사분면 (빨)
     gl.viewport(0, height, width, height);
     gl.scissor(0, height, width, height);
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);  // 빨간색
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // 3사분면 (왼쪽 아래, 파란색)
+    // 3사분면 (파)
     gl.viewport(0, 0, width, height);
     gl.scissor(0, 0, width, height);
-    gl.clearColor(0.0, 0.0, 1.0, 1.0);  // 파란색
+    gl.clearColor(0.0, 0.0, 1.0, 1.0); 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // 4사분면 (오른쪽 아래, 노란색)
+    // 4사분면 (노)
     gl.viewport(width, 0, width, height);
     gl.scissor(width, 0, width, height);
-    gl.clearColor(1.0, 1.0, 0.0, 1.0);  // 노란색
+    gl.clearColor(1.0, 1.0, 0.0, 1.0); 
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
